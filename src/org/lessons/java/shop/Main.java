432e15063852300e9b5d0508d61cc6282e53d466
CarloColletti~ <carlocolletti5@gmail.com>
2023-06-05 17:14:57 +0200

    first commit

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,18 +1,57 @@
 package org.lessons.java.shop;
 
 import java.text.DecimalFormat;
+import java.util.Scanner;
+
 
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Nome Prodotto: ");
+        String name =scan.nextLine();
+        System.out.print("Descrizione Prodotto: ");
+        String description =scan.nextLine();
+        System.out.print("Prezzo del Prodotto: ");
+        double price = Double.parseDouble(scan.nextLine());
+        System.out.print("Iva: ");
+        double iva = Double.parseDouble(scan.nextLine());
+
+        prodotto product = new prodotto(name, description, price, iva);
+
+
+        System.out.println("Codice: " + product.getCode());
+        System.out.println("Nome: " + product.getName());
+        System.out.println("Descrizione: " + product.getDescription());
+        System.out.println("Prezzo: " + product.getPrice());
+        System.out.println("Prezzo con IVA: " + product.getIvaPrice());
+        System.out.println("Nome esteso: " + product.getExtendedName());
+
+
+        System.out.print("Nuovo Nome: ");
+        String newName =scan.nextLine();
+        product.setName(newName);
+
+        System.out.print("Nuova Descrizione: ");
+        String newDescription =scan.nextLine();
+        product.setDescription(newDescription);
+
+        System.out.print("Nuovo Prezzo: ");
+        double newPrice = Double.parseDouble(scan.nextLine());
+        product.setPrice(newPrice);
+
+        System.out.print("Nuova Iva: ");
+        double newIva = Double.parseDouble(scan.nextLine());
+        product.setIva(newIva);
+
+        System.out.println("Nuovo nome: " + product.getName());
+        System.out.println("Nuova descrizione: " + product.getDescription());
 
 
 
-
-
-
-
-
+        scan.close();
     }
 
 }
--- src/org/lessons/java/shop/prodotto.java
+++ src/org/lessons/java/shop/prodotto.java
@@ -3,13 +3,63 @@ package org.lessons.java.shop;
 import java.util.Random;
 
 public class prodotto {
-    //attributi
-    private int codice;
-    private String nome;
-    private String descrizione;
-    private double prezzo;
-    private int iva;
+    private int code;
+    private String name;
+    private String description;
+    private double price;
 
+    private double iva;
 
+    public prodotto(String name, String description, double price, double iva) {
+        this.code = generateRandomCode();
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
 
+    private int generateRandomCode() {
+        Random random = new Random();
+        return random.nextInt(1000);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getIvaPrice() {
+        return price + (price * iva/100);
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public String getExtendedName() {
+        return code + "-" + name;
+    }
 }
